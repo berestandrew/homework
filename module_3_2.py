57a278b723a99afb4aaf85c85d8dc911df9b1844
berestandrew <97721941+berestandrew@users.noreply.github.com>
2024-12-04 16:22:59 +0700

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,24 @@
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+    ends = ['.com', '.ru', '.net']
+    if not ('@' in sender and '@' in recipient):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif not ((sender[-4:] in ends or sender[-3:] in ends) and (recipient[-3:] in ends or recipient[-4:] in ends)):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
+# Письмо успешно отправлено с адреса university.help@gmail.com на адрес vasyok1337@gmail.com
+# НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса urban.info@gmail.com на адрес urban.fan@mail.ru
+# Невозможно отправить письмо с адреса urban.teacher@mail.uk на адрес urban.student@mail.ru
+# Нельзя отправить письмо самому себе!
