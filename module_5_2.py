f687056fd8978476c4a452a70c6b77e1d9303ea6
berestandrew <97721941+berestandrew@users.noreply.github.com>
2024-12-25 15:38:21 +0700

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,30 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or 1 > new_floor:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h3 = House('ЖК Отражение', 25)
+
+print(h1)
+print(h2)
+print(h3)
+
+print(len(h1))
+print(len(h2))
+print(len(h3))
