3af064095b82a58b4d5d549a35a8ad069a68a083
berestandrew <97721941+berestandrew@users.noreply.github.com>
2025-01-03 14:50:42 +0700

    Add files via upload

---
+++ module_6_2.py
@@ -0,0 +1,59 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, __model, __engine_power, __color):
+        self.owner = owner
+        self.__model = __model
+        self.__engine_power = __engine_power
+        self.__color = __color
+
+    def get_model(self):
+        return f'Модель: {self.__model}'
+
+    def get_horse_power(self):
+        return f'Мощность двигателя: {self.__engine_power}'
+
+    def get_color(self):
+        return f'Цвет: {self.__color}'
+
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horse_power())
+        print(self.get_color())
+        print(f'Владелец: {self.owner}')
+
+    def set_color(self, new_color):
+        self.new_color = new_color
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
+
+# Вывод на консоль:
+# Модель: Toyota Mark II
+# Мощность двигателя: 500
+# Цвет: blue
+# Владелец: Fedos
+# Нельзя сменить цвет на Pink
+# Модель: Toyota Mark II
+# Мощность двигателя: 500
+# Цвет: BLACK
+# Владелец: Vasyok
